5ce5695f0b745244b3861571273bf7ae90bf5977
Anastasia27041 <anastasiaglyvka@gmail.com>
2023-04-09 14:50:11 +0300

    fixed task2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -5,15 +5,31 @@ const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 const STORAGE_KEY = 'videoplayer-current-time';
 
-player.on(
-  'timeupdate',
-  throttle(function (time) {
-    localStorage.setItem(STORAGE_KEY, time.seconds);
-  }, 1000)
-);
+player.on('timeupdate', throttle(onPlay, 1000));
 
-player.setCurrentTime(JSON.parse(localStorage.getItem(STORAGE_KEY)));
+function onPlay(time) {
+  localStorage.setItem(STORAGE_KEY, time.seconds);
+}
 
 player.getVideoTitle().then(function (title) {
   console.log('title:', title);
 });
+
+const currentTime = JSON.parse(localStorage.getItem(STORAGE_KEY));
+
+player
+  .setCurrentTime(currentTime)
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,9 @@
+import throttle from 'lodash.throttle';
+const form = document.querySelector('.feedback-form');
 
+form.addEventListener('input', throttle(onTextArea, 500));
+form.addEventListener('submit', onSubmit);
+
+function onSubmit(evt) {
+  evt.preventDefault();
+}
