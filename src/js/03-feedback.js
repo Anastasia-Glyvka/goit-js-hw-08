d7191b9af14b14a185a832016984c987718dd29f
Anastasia27041 <anastasiaglyvka@gmail.com>
2023-04-09 17:26:09 +0300

    task3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,9 +1,30 @@
 import throttle from 'lodash.throttle';
+
 const form = document.querySelector('.feedback-form');
+
+const STORAGE_KEY = 'feedback-form-state';
+
+const formData = {};
 
 form.addEventListener('input', throttle(onTextArea, 500));
 form.addEventListener('submit', onSubmit);
 
+populateTextarea();
+
 function onSubmit(evt) {
-  evt.preventDefault();
+  evt.preventDefault(evt.currentTarget);
+  evt.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
 }
+function onTextArea(evt) {
+  formData[evt.target.name] = evt.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+// function populateTextarea(evt) {
+//   const savedData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+
+//   if (savedData) {
+
+//   }
+// }
