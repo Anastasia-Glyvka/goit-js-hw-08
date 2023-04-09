155320a7e6aeacd5418e0c25bbce9c349f6f0a4b
Anastasia27041 <anastasiaglyvka@gmail.com>
2023-04-09 13:57:52 +0300

    task2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,20 @@
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const STORAGE_KEY = 'videoplayer-current-time';
+
+player.on(
+  'timeupdate',
+  throttle(function (time) {
+    localStorage.setItem(STORAGE_KEY, time.seconds);
+  }, 1000)
+);
+console.log(
+  player.setCurrentTime(JSON.parse(localStorage.getItem(STORAGE_KEY)))
+);
+
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
