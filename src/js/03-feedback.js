ab325f1d3667e26e8064172d6241f14a1e8df8cf
Anastasia27041 <anastasiaglyvka@gmail.com>
2023-05-04 13:24:07 +0300

    fixed task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,40 +1,30 @@
 import throttle from 'lodash.throttle';
 
-const form = document.querySelector('.feedback-form');
-const STORAGE_KEY = 'feedback-form-state';
-const savedData = JSON.parse(localStorage.getItem(STORAGE_KEY));
-const formData = {};
+//
+const form = document.querySelector('form');
+const KEY = 'feedback-form-state';
+let data = JSON.parse(localStorage.getItem(KEY)) ?? {};
 
-form.addEventListener('input', throttle(onTextArea, 500));
 form.addEventListener('submit', onSubmit);
-
-populateTextarea();
+form.addEventListener('input', onInput);
 
 function onSubmit(evt) {
-  console.log(form.elements);
   evt.preventDefault();
-  if (form.elements.email.value === '' || form.elements.message.value === '') {
-    alert('Введіть дані');
-  } else {
-    const data = {
-      email: form.elements.email.value,
-      message: form.elements.message.value,
-    };
-    console.log(data);
-  }
-
+  console.log(data);
+  data = {};
+  localStorage.removeItem(KEY);
   evt.currentTarget.reset();
-  localStorage.removeItem(STORAGE_KEY);
 }
 
-function onTextArea(evt) {
-  formData[evt.target.name] = evt.target.value;
-  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
-}
+(function () {
+  const { email, message } = form.elements;
+  email.value = data.email || '';
+  message.value = data.message || '';
+})();
 
-function populateTextarea() {
-  if (savedData) {
-    form.elements.email.value = savedData.email;
-    form.elements.message.value = savedData.message;
-  }
+function onInput(evt) {
+  const { name, value } = evt.target;
+  data[name] = value;
+
+  localStorage.setItem(KEY, JSON.stringify(data));
 }
